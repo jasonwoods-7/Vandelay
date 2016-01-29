4207ba81be24a34e37dcd8b89a7cf8b4409e58e3
jasonwoods-7 <jwoods@appliedsystems.com>
2016-01-29 08:40:56 -0600

    Removing Tail.Fody because it causes invalid program when compiled in release mode.

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.1.1")]
-[assembly: AssemblyFileVersion("0.2.1.1")]
+[assembly: AssemblyVersion("0.2.1.2")]
+[assembly: AssemblyFileVersion("0.2.1.2")]
