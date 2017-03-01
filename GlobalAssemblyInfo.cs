a2151279b26d9ecb39f16b41bbf22ddb60c624b6
Jason Woods <jwoods@appliedsystems.com>
2017-03-01 14:15:28 -0600

    Bump version

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.2.1")]
-[assembly: AssemblyFileVersion("0.2.2.1")]
-[assembly: AssemblyCopyright("Copyright © 2015-2016")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyFileVersion("0.2.3.0")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017")]
