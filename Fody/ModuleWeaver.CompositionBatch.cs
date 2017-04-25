99004f5a113b9e7e032e6558e01dbfafb432a5ea
Jason Woods <jwoods@appliedsystems.com>
2017-04-25 11:53:54 -0500

    PEVerify fix

--- Fody/ModuleWeaver.CompositionBatch.cs
+++ Fody/ModuleWeaver.CompositionBatch.cs
@@ -57,108 +57,117 @@ namespace Vandelay.Fody
         ModuleDefinition.TypeSystem.Object));
       compositionBatch.Body.Variables.Add(new VariableDefinition(
         ModuleDefinition.ImportReference(typeof(Type))));
+      compositionBatch.Body.InitLocals = true;
 
+      foreach (var instruction in GetInstructions())
+      {
+        compositionBatch.Body.Instructions.Add(instruction);
+      }
+
+      return compositionBatch;
+    }
+
+    IEnumerable<Instruction> GetInstructions()
+    {
       // var compositionBatch = new CompositionBatch();
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Newobj,
+      yield return Instruction.Create(OpCodes.Newobj,
         ModuleDefinition.ImportReference(typeof(CompositionBatch)
-        .GetConstructor(new Type[0]))));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Stloc_0));
+        .GetConstructor(new Type[0])));
+      yield return Instruction.Create(OpCodes.Stloc_0);
 
       // var i = 0;
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4_0));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Stloc_1));
+      yield return Instruction.Create(OpCodes.Ldc_I4_0);
+      yield return Instruction.Create(OpCodes.Stloc_1);
 
       var loopConditionCheck = Instruction.Create(OpCodes.Ldloc_1);
 
       // goto loopConditionCheck;
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Br_S,
-        loopConditionCheck));
+      yield return Instruction.Create(OpCodes.Br_S,
+        loopConditionCheck);
 
       // loopStart:
       // var obj = array[i];
       var loopStart = Instruction.Create(OpCodes.Ldarg_0);
-      compositionBatch.Body.Instructions.Add(loopStart);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_1));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldelem_Ref));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Stloc_2));
+      yield return loopStart;
+      yield return Instruction.Create(OpCodes.Ldloc_1);
+      yield return Instruction.Create(OpCodes.Ldelem_Ref);
+      yield return Instruction.Create(OpCodes.Stloc_2);
 
       // var type = obj.GetType();
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_2));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Callvirt,
-        ModuleDefinition.ImportReference(typeof(object).GetMethod("GetType"))));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Stloc_3));
+      yield return Instruction.Create(OpCodes.Ldloc_2);
+      yield return Instruction.Create(OpCodes.Callvirt,
+        ModuleDefinition.ImportReference(typeof(object).GetMethod("GetType")));
+      yield return Instruction.Create(OpCodes.Stloc_3);
 
       // var contractName = AttributedModelServices.GetContractName(type);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_0));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_3));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Call,
+      yield return Instruction.Create(OpCodes.Ldloc_0);
+      yield return Instruction.Create(OpCodes.Ldloc_3);
+      yield return Instruction.Create(OpCodes.Call,
         ModuleDefinition.ImportReference(typeof(AttributedModelServices)
-        .GetMethod("GetContractName"))));
+        .GetMethod("GetContractName")));
 
       // var metaData = new Dictionary<string, object>
       // {
       //   ["ExportTypeIdentity"] = AttributedModelServices.GetTypeIdentity(type)
       // }
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Newobj,
+      yield return Instruction.Create(OpCodes.Newobj,
         ModuleDefinition.ImportReference(typeof(Dictionary<string, object>)
-        .GetConstructor(new Type[0]))));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Dup));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldstr,
-        "ExportTypeIdentity"));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_3));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Call,
+        .GetConstructor(new Type[0])));
+      yield return Instruction.Create(OpCodes.Dup);
+      yield return Instruction.Create(OpCodes.Ldstr,
+        "ExportTypeIdentity");
+      yield return Instruction.Create(OpCodes.Ldloc_3);
+      yield return Instruction.Create(OpCodes.Call,
         ModuleDefinition.ImportReference(typeof(AttributedModelServices)
-        .GetMethod("GetTypeIdentity", new[] { typeof(Type) }))));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Callvirt,
+        .GetMethod("GetTypeIdentity", new[] { typeof(Type) })));
+      yield return Instruction.Create(OpCodes.Callvirt,
         ModuleDefinition.ImportReference(typeof(Dictionary<string, object>)
-        .GetProperty("Item").GetSetMethod())));
+        .GetProperty("Item").GetSetMethod()));
 
       // var valueProvider = new ExportValueProvider(obj);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_2));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Newobj,
-        ExportValueProvider.GetConstructors().First()));
+      yield return Instruction.Create(OpCodes.Ldloc_2);
+      yield return Instruction.Create(OpCodes.Newobj,
+        ExportValueProvider.GetConstructors().First());
 
       // var valueFunc = new Func<object>(valueProvider.GetValue);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldftn,
-        ExportValueProvider.Methods.First(m => !m.IsConstructor)));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Newobj,
+      yield return Instruction.Create(OpCodes.Ldftn,
+        ExportValueProvider.Methods.First(m => !m.IsConstructor));
+      yield return Instruction.Create(OpCodes.Newobj,
         ModuleDefinition.ImportReference(typeof(Func<object>)
-        .GetConstructor(new[] { typeof(object), typeof(IntPtr) }))));
+        .GetConstructor(new[] { typeof(object), typeof(IntPtr) })));
 
       // var export = new Export(contractName, metaData, valueFunc);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Newobj,
+      yield return Instruction.Create(OpCodes.Newobj,
         ModuleDefinition.ImportReference(typeof(Export).GetConstructor(new[]
         {
           typeof(string),
           typeof(Dictionary<string, object>),
           typeof(Func<object>)
-        }))));
+        })));
 
       // compositionBatch.AddExport(export);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Callvirt,
+      yield return Instruction.Create(OpCodes.Callvirt,
         ModuleDefinition.ImportReference(typeof(CompositionBatch)
-        .GetMethod("AddExport", new[] { typeof(Export) }))));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Pop));
+          .GetMethod("AddExport", new[] { typeof(Export) })));
+      yield return Instruction.Create(OpCodes.Pop);
 
       // i++;
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_1));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldc_I4_1));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Add));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Stloc_1));
+      yield return Instruction.Create(OpCodes.Ldloc_1);
+      yield return Instruction.Create(OpCodes.Ldc_I4_1);
+      yield return Instruction.Create(OpCodes.Add);
+      yield return Instruction.Create(OpCodes.Stloc_1);
 
       // loopConditionCheck:
       // if (i < array.Length) goto loopStart;
-      compositionBatch.Body.Instructions.Add(loopConditionCheck);
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldarg_0));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldlen));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Conv_I4));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Blt_S, loopStart));
+      yield return loopConditionCheck;
+      yield return Instruction.Create(OpCodes.Ldarg_0);
+      yield return Instruction.Create(OpCodes.Ldlen);
+      yield return Instruction.Create(OpCodes.Conv_I4);
+      yield return Instruction.Create(OpCodes.Blt_S, loopStart);
 
       // return compositionBatch;
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ldloc_0));
-      compositionBatch.Body.Instructions.Add(Instruction.Create(OpCodes.Ret));
-
-      return compositionBatch;
+      yield return Instruction.Create(OpCodes.Ldloc_0);
+      yield return Instruction.Create(OpCodes.Ret);
     }
   }
 }
